72d4c371da43bd5cbd26411178f14e2d8a791778
unknown <gub.cse.files@gmail.com>
2019-04-10 02:33:07 +0600

    Commit after importing from Bitbucket

---
+++ GADM/src/com/gadm/GADM.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gadm;
+
+import com.gadm.momtaz.Save_Level_0;
+
+/**
+ *
+ * @author Abedeen created
+ */
+public class GADM
+    {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+        {
+            System.out.println("Alhumdulial...!");
+            Save_Level_0 level_0 =new Save_Level_0();
+            level_0.saveData();
+            
+        }//main    
+    }//class
---
+++ GADM/src/com/gadm/util/CSV_Folder_Location_Util.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gadm.util;
+
+/**
+ *
+ * @author abedeen
+ */
+public class CSV_Folder_Location_Util
+    {
+        public static String folderLocation = "GADM_ALL_CSV\\GADM_ALL_CSV\\";
+    }//class
